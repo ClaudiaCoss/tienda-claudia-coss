51348dac9989bcdc7d8447de8b63fbb5da484e33
Claudia Coss <claudiacoss17@gmail.com>
2025-06-17 19:41:04 -0600

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import ItemListContainer from './components/ItemListContainer.jsx'
 import ItemCount from './components/ItemCount.jsx'
 import NavBarBoots from './components/NavBarBoots.jsx'
+import ItemDetailContainer from './components/ItemDetailContainer.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ErrorPage from './components/ErrorPage.jsx'
 function App() {
 
 
   return (
-    <>
-      <NavBarBoots/>
-      <ItemListContainer saludo='Dulces Mexicanos para toda ocasión' />
-      <ItemCount stock={200}/>
-      
-    </>
+    <BrowserRouter>
+      <NavBarBoots />
+      <Routes>
+        <Route path='/' element={<ItemListContainer saludo='Dulces Mexicanos para toda ocasión' />} />
+        <Route path='/item' element={<ItemListContainer/>} />
+        <Route path='*' element={<ErrorPage/>} />
+      </Routes>
+      {/* <ItemCount stock={200} /> */}
+
+
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h2>Lo siento, la página no existe.</h2>
+    </div>
+  )
+}
+
+export default ErrorPage
